33a8f69ffef5bd47971aab60e4d418ab82c977ce
Viktor Karpinski <viktorkarpinski@Viktors-MacBook-Pro-2.local>
2022-03-06 14:10:15 +0100

    1.2; Dashboard HTML frame done;

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,87 @@
 @extends('master')
 
 @section('content')
-    {{ $user->name }}
 
-    <a href="/logout/">logout</a>
+<header>
+    <button id="add-new">
+        <span>
+            +
+        </span>
+    </button>
+
+    <aside>
+        <h2>
+            {{ $user->name }}
+        </h2>
+
+        <a href="/logout/">
+            Sign Out
+        </a>
+    </aside>
+</header>
+
+<section id="main">
+    <aside>
+        <div class="side-box"></div>
+        <div class="side-box"></div>
+        <div class="side-box"></div>
+        <div class="side-box"></div>
+    </aside>
+
+    <article>
+        <ul>
+            <li>
+                DATE 
+                <span></span>
+            </li>
+             <li>
+                TYPE 
+                <span></span>
+            </li>
+             <li>
+                CATEGORY 
+                <span></span>
+            </li>
+             <li>
+                TIME 
+                <span></span>
+            </li>
+             <li>
+                NOTES 
+                <span></span>
+            </li>
+             <li>
+                TAGS 
+                <span></span>
+            </li>
+            <li>
+
+            </li>
+        </ul>
+         <ul>
+            <li>
+                placeholder 
+            </li>
+             <li>
+                placeholder 
+            </li>
+             <li>
+                placeholder 
+            </li>
+             <li>
+                placeholder 
+            </li>
+             <li>
+                placeholder 
+            </li>
+             <li>
+                placeholder 
+            </li>
+            <li>
+                
+            </li>
+        </ul>
+    </article>
+</section>
+
 @endsection
